56a518def79f105f055c34cd4cf1b425b5e779d3
glenbuck503 <glenbuck503@gmail.com>
2021-02-05 09:44:30 -0800

    create template

---
+++ _tests_/template.test.js
@@ -0,0 +1,7 @@
+import template from '../src/js/template.js';
+
+describe('template', () => {
+
+  
+
+});
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import template from './js/triangle.js';
+i
+
+$(document).ready(function() {
+  $('#template').submit(function(event) {
+   
+ });
+});
