aebc270fc27583226b48c716c478ff5344c190e5
glenbuck503 <glenbuck503@gmail.com>
2021-02-05 10:13:16 -0800

    bad test for constructor

--- __tests__/space.tests.js
+++ __tests__/space.tests.js
@@ -4,4 +4,10 @@ describe('Person', () => {
   let person; beforeEach(() => {
     person = new Person ("GlenBuck", 36);
   });
+
+  test('should take user input and create object', () => {
+    expect(person.name).toEqual("GlenBuck");
+    expect(person.age).toEqual(35);
+  });
+
 });
