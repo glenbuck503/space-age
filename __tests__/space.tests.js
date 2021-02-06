a78bdc4a7e65d4af8f994eb4535f3e7d3c88c4bc
glenbuck503 <glenbuck503@gmail.com>
2021-02-06 14:08:26 -0800

    bad test for showDisplayMars for years left

--- __tests__/space.tests.js
+++ __tests__/space.tests.js
@@ -82,5 +82,12 @@ describe('Person', () => {
     let exp = marsPast.marsAge() - 75; 
     expect(marsPast.showDisplayMars()).toEqual(` Your age on Mars is : ${age} You have lived ${exp * -1} years past the life expectancy on Mars`);
   }); 
+
+  test('should display user years left on Mars', () => {
+    let marsleft = new Person ("GlenBuck", 78)
+    let age = marsleft.marsAge(); 
+    let exp = marsleft.marsAge() - 75; 
+    expect(marsleft.showDisplayMars()).toEqual(` Your age on Mars is : ${age}. You still have ${exp * -1} years on Mars`);
+  }); 
 });
   
